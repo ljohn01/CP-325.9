82c564658bbac1ceb9d86597411a01b3b61a35cb
ljohn01 <01.lauren.johnson@gmail.com>
2024-05-03 12:09:51 -0400

    inital commit (working navbar)

---
+++ blog-music/src/components/Navbar.jsx
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { FaMusic } from "react-icons/fa"
+import { FaBars, FaTimes } from 'react-icons/fa'
+import { Button } from '../components/Button'
+import './Navbar.css'
+import { IconContext } from 'react-icons/lib'
+
+function Navbar() {
+    const [click, setClick] = useState(false)
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true)
+        }
+    };
+
+    useEffect(() => {
+        showButton();
+    }, []);
+
+    window.addEventListener('resize', showButton);
+
+
+    return (
+        <>
+        <IconContext.Provider value={{ color: '#fff'}}>
+            <div className='navbar'>
+                <div className='navbar-container container'>
+                    <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+                        <FaMusic className='navbar-icon' />
+                        Harmonic Haven
+                    </Link>
+                    <div className='menu-icon' onClick={handleClick}>
+                        {click ? <FaTimes /> : <FaBars />}
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className="nav-item">
+                            <Link to='/' className="nav-links" onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/blog' className="nav-links" onClick={closeMobileMenu}>
+                                Blog
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/about' className="nav-links" onClick={closeMobileMenu}>
+                                About
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/profile' className="nav-links" onClick={closeMobileMenu}>
+                                Profile
+                            </Link>
+                        </li>
+                        <li className="nav-btn">
+                            {button ? (
+                                <Link to='/sign-up' className="btn-link">
+                                    <Button buttonStyle='btn--outline'>SIGN UP</Button>
+                                </Link>
+                            ): (
+                                <Link to='/sign-up' className="btn-link" onClick={closeMobileMenu}>
+                                    <Button buttonStyle='btn--outline'
+                                        buttonSize='btn--mobile'>
+                                    SIGN UP</Button>
+                                </Link>
+                            )}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            </IconContext.Provider>
+        </>
+    )
+}
+
+export default Navbar
