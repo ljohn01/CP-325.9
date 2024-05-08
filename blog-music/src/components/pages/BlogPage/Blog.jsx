9a9e3e968c9618f0a7b3d6216c6a3dd66fed5efe
ljohn01 <01.lauren.johnson@gmail.com>
2024-05-08 11:02:52 -0400

    another commit

--- blog-music/src/components/pages/BlogPage/Blog.jsx
+++ blog-music/src/components/pages/BlogPage/Blog.jsx
@@ -35,19 +35,22 @@ function Blog() {
 
             <div className="post">
                 <div className="image">
-                <img src="https://images.unsplash.com/photo-1527150122806-f682d2fd8b09?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt=""/>
+                <img src="https://images.unsplash.com/photo-1511379938547-c1f69419868d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt=""/>
                 </div>
 
                 <div className="texts">
                 <h2>How To Find Your Perfect Music Genre</h2>
                 <p className="info">
-                    <a className="author">shyguy04</a>
+                    <a className="author">musicguy04</a>
                     <time>2024-05-07 11:46</time>
                 </p>
                 <p className="summary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium illo tenetur, corrupti dolorum eos ipsa nulla fugit, deleniti debitis quia labore sint aspernatur! Similique eligendi nam dignissimos, nulla omnis quo?</p>
                 </div>
             </div>
 
+            <a href='/create'>
+            <button>Create a blog post</button>
+            </a>
         </main>
     )
 }
